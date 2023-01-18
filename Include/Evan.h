dfb6bb6b1486190c2f42556257a2afc0671ea80a
LE Dylan <dylan.le@ecole-89.com>
2023-01-18 14:25:38 +0100

    update

--- Include/Evan.h
+++ Include/Evan.h
@@ -1,10 +1,11 @@
 #ifndef EVAN_H_
 #define EVAN_H_
+#include <lapin.h>
 void clear_pixelarray(t_bunny_pixelarray *pxa, unsigned int color);
 void put_pixel(t_bunny_pixelarray * pxa,
                t_bunny_position *pos,
                unsigned int color);
-float get_ratio(int a, int b, int x);
-int get_value(int a,int b, float ratio);
+double deg_to_rads(int degrees);
 
-#endif /*EVAN*/
+
+#endif /*EVAN_H_*/
